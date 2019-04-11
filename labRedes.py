65a84277afc6d484de1bec9314e54a8b4bc1898b
Unknown <jorge.plaza@usach.cl>
2019-04-11 11:19:52 -0400

    Pequeño cambio

--- labRedes.py
+++ labRedes.py
@@ -5,13 +5,14 @@ import scipy.io.wavfile as wav
 import matplotlib.pyplot as plotFunction
 from scipy.fftpack import fft as fourier
 from scipy.fftpack import ifft as inverseFourier 
+from numpy import arange
 
 # Se lee el archivo de audio handel y se guarda
 # fs es la frecuencia
 # data es el valor de la amplitudes
 (fs, data) = wav.read('handel.wav')
 
-# Se genera los valores de las amplitudes en el tiempo
+# Se genera los valores de tiempo entre cada amplitud
 ejeX = np.linspace(0, len(data)/fs, num=len(data))
 
 # Se realiza grafico amplitud vs tiempo
@@ -27,11 +28,16 @@ fourierTransform = fourier(data)
 #Calculo de la inversa de la transformada
 inverseFourierTransform = inverseFourier(fourierTransform)
 
+#Calculo de las frecuencias (Eje X)
+k = arange(len(data))
+T = len(data) / fs
+frqLabel = k/T
+
 # Se grafica la transformada de fourier
-plotFunction.ylabel('frecuencia (w)')
-plotFunction.xlabel('Tiempo (s)')
+plotFunction.xlabel('frecuencia (w)')
+plotFunction.ylabel('F(w)')
 plotFunction.title('Grafico en el dominio de la frecuencia')
-plotFunction.plot(ejeX, fourierTransform)
+plotFunction.plot(frqLabel, fourierTransform)
 plotFunction.show()
 
 #Se grafica la inversa de la transformada
